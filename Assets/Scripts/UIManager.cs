5476996bc69cc0a107b52ebc1c9fb58dc8cd0977
DESKTOP-SJ6GBQC\Mert POLAT <mert.polat@sabanciuniv.edu>
2025-01-25 15:46:33 +0300

    Add upgrade panel functionality and UI updates

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,6 +14,23 @@ public class UIManager : MonoBehaviour
     [Header("Health")]
     public TextMeshProUGUI healthText;
 
+    [Header("Upgrades")]
+    public TextMeshProUGUI upgradeDescriptionText;
+
+
+    public GameObject upgradePanel;
+    public GameObject upgradePanelLayout;
+
+    public GameObject upgradeContainer;
+
+
+    public TextMeshProUGUI coinText;
+
+    public Image jumpCooldownImage;
+    
+
+    
+
     void Awake()
     {
         if (instance == null)
@@ -34,8 +51,47 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+   
+            ToggleUpgradePanel(!upgradePanel.activeSelf);
+        }
     }
+
+
+    public void UpdateJumpCooldown(float percent)
+    {
+        jumpCooldownImage.fillAmount = 1f- percent;
+    }
+    public void UpdateCoinText(int coins)
+    {
+        coinText.text = "Coins: " + coins;
+    }
+    public void ToggleUpgradePanel(bool state)
+    {
+        upgradePanel.SetActive(state);
+        if(state)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void UpdateUpgradeDescription(string upgradeDescription)
+    {
+        Debug.Log("Updating upgrade description");
+        Debug.Log(upgradeDescription); 
+        upgradeDescriptionText.text = upgradeDescription;
+    }
+
+    public void DisableUpgradeDescription()
+    {
+        upgradeDescriptionText.text = "";
+    }
+    
 
 
     public void UpdateHealthText(int health)
